dbb332a89feb8d5d161fd18f87b90354259c2457
Alany-b <alangimenezz14@gmail.com>
2025-08-27 16:27:01 -0300

    Controladores terminados

--- src/controllers/user-controllers.js
+++ src/controllers/user-controllers.js
@@ -8,7 +8,7 @@ export const getAllUsers = async (req, res) => {
         const users = await user.findAll();
         res.status(200).json(users);
     } catch (error) {
-        res.status(500).json({ message: "Error retrieving users", error });
+        res.status(500).json({ message: "No existen los usuarios", error });
     }
 };
 // Create a new user
@@ -40,31 +40,64 @@ export const update = async (req, res) => {
     const { id } = req.params;
     const { name, email, password } = req.body;
     try {
-        const [updated] = await user.update(
-            { name, email, password },
-            { where: { id } }
-        );
-        if (updated) {
-            const updatedUser = await user.findByPk(id);
-            res.status(200).json(updatedUser);
-        } else {
-            res.status(404).json({ message: "User not found" });
-        }
-    } catch (error) {
-        res.status(500).json({ message: "Error updating user", error });
+         const { name, email, password } = req.body;
+    if (
+      name === "" ||
+      name === undefined ||
+      email === "" ||
+      email === undefined ||
+      password === "" ||
+      password === undefined
+    )
+      return res
+        .status(400)
+        .json({ message: "Los campos no deben estar vacios" });
+
+    if (name.length > 100)
+      return res
+        .status(400)
+        .json({ message: "El nombre no debe tener más de 100 caracteres" });
+
+    if (email.length > 100)
+      return res
+        .status(400)
+        .json({ message: "El email no debe tener más de 100 caracteres" });
+
+    const emailExiste = await User.findOne({
+      where: { email: email, id: { [Op.ne]: req.params.id } },
+    });
+    if (emailExiste)
+      return res.status(400).json({ message: "Ya existe el email" });
+
+    if (password.length > 100)
+      return res
+        .status(400)
+        .json({ message: "El password no debe tener más de 100 caracteres" });
+
+    const [update] = await User.update(req.body, {
+      where: { id: req.params.id },
+    });
+    if (update) {
+      const actualizarUser = await User.findByPk(req.params.id);
+      return res.status(200).json(actualizarUser);
+    } else {
+      return res.status(404).json({ message: "Usuario no encontrado" });
     }
-}
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+};
 // Delete a user by ID
 export const deleteUser = async (req, res) => {
     const { id } = req.params;
     try {
-        const deleted = await user.destroy({ where: { id } });
-        if (deleted) {
-            res.status(204).send();
-        } else {
-            res.status(404).json({ message: "User not found" });
-        }
-    } catch (error) {
-        res.status(500).json({ message: "Error deleting user", error });
-    }
-}
+          const eliminarUsusario = await User.destroy({
+      where: { id: req.params.id },
+    });
+    if (!eliminarUsusario)
+      return res.status(404).json({ message: "El usuario no existe" });
+    return res.status(200).json({ message: "Usuario eliminado" });
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+};
